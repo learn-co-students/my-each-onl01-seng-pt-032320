c5ac35816af757ad8c899fb98bf55c1e3eb58136
Allan Farca <allan.farca@gmail.com>
2020-03-31 04:23:08 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,16 @@
-def my_each # put argument(s) here
-  # code here
+array = [00, 01, 10, 11, 100, 101, 110, 111]
+
+def my_each (array)
+if block_given?
+    i = 0
+ 
+    while i < array.length
+      yield(array[i])
+      i = i + 1
+    end
+ 
+    array
+  else
+    return nil
+  end
 end
