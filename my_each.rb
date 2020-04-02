c976719d97ce897ad8db1a24802ff532d4f0d331
Dina Kendle <dina.kendle@gmail.com>
2020-04-02 18:33:35 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -2,10 +2,10 @@ def my_each(array)
   i = 0
   
   while i < array.length 
-    yield array[i]
+    yield array[i] #While i is less than the array length or the total amount of items in the array, it will be passed onto i =  so that the value can be captured and then ran through the (i = i+1)
       i = i + 1
   end
-array
+array #Tell's the method to return the original array
 end
  
 
